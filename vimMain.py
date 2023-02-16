4bcc9e9324547e05a746c167de2f503ee684d7c0
Pritam <84720825+pritam12426@users.noreply.github.com>
2023-02-16 17:55:07 +0530

    Vim only navigation practice
    * NOW THIS IS ABLE TO NAVIGATION PRACTICE

---
+++ vimMain.py
@@ -0,0 +1,40 @@
+import os
+import random
+from playsound import playsound
+print(' ⬆️ --> k\n', '⬇️ --> j\n', '➡️ --> l\n', '⬅️ --> h')
+
+tole = 0
+error = 0
+
+while True:
+    l = ['⬆️', '⬇️', '➡️', '⬅️']
+    random.shuffle(l)
+    l = l[0]
+    print("-----------------------------------------------")
+    print(f'\n  **** {l}  ****\n')
+    j = input("Inset > ")
+
+    if j == "i":
+        
+        print(f'\n**** Result **** \nTotal attempt > {tole}\nTotal error > {error}\nCorrect attempt > {tole-error}\n******************\n')
+        exit()
+    elif l == '⬆️' and j.lower() != "k":
+        playsound("Error.wav")
+        error +=1
+
+    elif l == '⬇️' and j.lower() != "j":
+        playsound("Error.wav")
+        print('\t**** j ****')
+        error +=1
+
+    elif l == '➡️' and j.lower() != "l":
+        playsound("Error.wav")
+        print('\t**** l ****')
+        error +=1
+
+    elif l == '⬅️' and j.lower() != "h":
+        playsound("Error.wav")    
+        print('\t**** h ****')
+        error +=1
+
+    tole +=1
