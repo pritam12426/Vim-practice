0d0ee8b4cd9e6b549a1de19f2ce0af0aca5f411f
Pritam <84720825+pritam12426@users.noreply.github.com>
2023-02-19 13:39:22 +0530

    This is a inprove version of key

--- key.py
+++ key.py
@@ -1,27 +1,57 @@
+import json
+import os
 import playsound
 import random
-n = 0
+
+# with open("shortKey.json")as read:
+#     shortKey = read.read()
+
+#     print(shortKey)
+
+os.system("clear")
 
 totle = 0
 error = 0
 
-while True:
-    keyName = ['Exit wihtout saving', 'Save and exit', 'Just exit', 'Just save', 'Jump forword', 'Jump back', 'Delete iner word', 'Delete single word']
-    shortKey = [':q!', ':wq', ':q', ':w', 'w', 'b', 'diw', 'dw', ]
+keyName = ['Exit with out saving',
+           'Save and exit', 
+           'Just exit',
+           'Just save',
+           'Jump forword',
+           'Jump back',
+           'Delete iner word',
+           'Delete single word',
+           'Open Neo_vim']
 
-    a = random.randint(0, 7)
+shortKey = [':q!',
+            ':wq',
+            ':q',
+            ':w',
+            'w',
+            'b',
+            'diw',
+            'dw',
+            'nvim .']
+
+while True:
+    a = random.randint(0, len(keyName)-1)
     l = keyName[a]
 
-    print("-------------------")
-    print(f'**** {l} ****')
+    b = (f'**** {l} ****')
+    print(b)
     j = input("Inset > ")
 
-    if j.lower().find('i') == 0:
-        exit(f'\n**** Result **** \nTotal attempt > {totle}\nTotal error > {error}\nCorrect attempt > {totle-error}\n******************\n')
-
+    if j.lower().find('exit') == 0:
+        os.system("clear")
+        exit(f'**** Result **** \nTotal attempt > {totle}\nTotal error > {error}\nCorrect attempt > {totle-error}\n'+"*"*22)
+    elif j.lower().find("clear") == 0:
+        os.system("clear")
     elif j != shortKey[a]:
-        print(f'**** 🤬️ ****')
-        print(shortKey[a])
+        print('**** 🤬️ ****')
+        print(f'Ans > {shortKey[a]}')
+        print("=" *len(b))
         playsound.playsound("Error.wav")
         error +=1
-    totle +=1
+    else:
+        totle +=1
+        print("=" *len(b))
