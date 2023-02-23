c5c1e9f5bbdddb2b3c5d7861eec77419f3c4e243
Pritam <84720825+pritam12426@users.noreply.github.com>
2023-02-23 09:43:41 +0530

    Now the file will take keybinding input form json

--- shortCut.py
+++ shortCut.py
@@ -4,65 +4,19 @@ import playsound
 import colorsys
 import random
 
-n = random.randint(1, 99)
-
 os.system("clear")
 
 totle = 0
 error = 0
 
-keyName = ['Exit with out saving',
-           'Save and exit', 
-           'Just exit',
-           'Just save',
-           'Jump forword',
-           'Jump back',
-           'Delete iner word',
-           'Delete single word',
-           'Open Neo_vim here',
-           'Undo',
-           'Delete iner ""', 
-           'Delete iner ()',
-           'Change whole line',
-           'Change iner word',
-           'Jump end of the word',
-           f'Jump to line number {n}',
-           'Jump to ending of the line',
-           'Jump to starting of the line',
-           'Jump to begning of the file',
-           'Jump to ending of the file']
-
-shortKey = [':q!',
-            ':wq',
-            ':q',
-            ':w',
-            'w',
-            'b',
-            'diw',
-            'dw',
-            'nvim .',
-            'u',
-            'di"',
-            'di(',
-            'cc',
-            'ciw',
-            'e',
-            f'{n}g',
-            '0',
-            '$',
-            'gg',
-            'G']
-
-if len(shortKey) != len(keyName):
-    print(f"The len of word {len(keyName)}")
-    print(f"The len of short key {len(shortKey)}")
-    exit("Pls check the key it is not propre")
+json_file = open("shortKey.json")
+json_file = json.load(json_file)
 
 while(True):
-    a = random.randint(0, len(keyName)-1)
-    l = keyName[a]
+    jsonKey = list(json_file.keys())
+    jsonKey = random.choice(jsonKey)
 
-    b = (f'**** {l} ****')
+    b = (f'**** {jsonKey} ****')
     print(b)
     j = input("Inset > ")
 
@@ -73,9 +27,9 @@ while(True):
     elif j.lower().find("clear") == 0:
         os.system("clear")
 
-    elif j != shortKey[a]:
+    elif j != json_file[jsonKey]:
         print('**** 🤬️ ****')
-        print(f'Ans > {shortKey[a]}')
+        print(f'Ans > {json_file[jsonKey]}')
         print("=" * len(b))
         playsound.playsound("Error.wav")
         error +=1
